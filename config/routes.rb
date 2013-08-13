ae51b99df14cd4abe9b628eb35802a8220002fe9
Joakim Runeberg <joakim.runeberg@gmail.com>
2013-08-13 11:04:25 +0300

    routes again

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Minisite::Application.routes.draw do
-  root :to => "home/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,6 +49,7 @@ Minisite::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
