3b60bf10d690ea56752863fbfbf2cbed71ae4300
Joakim Runeberg <joakim.runeberg@gmail.com>
2013-08-13 10:45:22 +0300

    production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 # TODO: Set app name
 
-Example::Application.configure do
+Minisite::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
